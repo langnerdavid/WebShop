149438fba68ff873d761db2584559dd4a699b3eb
langnerdavid <david@MBP-von-David.fritz.box>
2023-12-02 14:33:32 +0100

    implement simple register form

--- basic-frontend/src/app/app-routing.module.ts
+++ basic-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,21 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
 
+import { RegisterComponent } from './pages/register/register.component';
+import { SigninComponent } from './pages/signin/signin.component';
+
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
   },
   {
     path: '404',
--- basic-frontend/src/app/app.module.ts
+++ basic-frontend/src/app/app.module.ts
@@ -11,12 +11,22 @@ import {FormsModule} from "@angular/forms";
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { RegisterComponent } from './pages/register/register.component';
+import { SigninComponent } from './pages/signin/signin.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { PasswordModule } from 'primeng/password';
+
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    RegisterComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +34,11 @@ import { MenubarModule } from 'primeng/menubar';
     FormsModule,
     InputTextModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
