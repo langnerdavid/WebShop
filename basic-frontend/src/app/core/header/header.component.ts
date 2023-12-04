78f3868bd85d86daf599d5dc305711743174be3c
langnerdavid <langner.david@outlook.com>
2023-12-04 18:11:26 +0100

    style header.component.css / footer.component.css better

--- basic-frontend/src/app/core/header/header.component.ts
+++ basic-frontend/src/app/core/header/header.component.ts
@@ -8,18 +8,6 @@ import {Component} from '@angular/core';
 export class HeaderComponent {
   searchText: string | undefined;
 
-  onProfileClick() {
-    console.log('Profil-Button geklickt');
-    //TODO
-    // weiterleiten auf Profilseite
-  }
-
-  onCartClick() {
-    console.log('Warenkorb-Button geklickt');
-    //TODO
-    // weiterleiten zum Warenkorb
-  }
-
   onSearch(){
     //TODO
     // SuchLogik -> beim drücken des Icons soll Suche abgeschlossen werden
