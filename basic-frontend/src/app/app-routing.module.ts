677a2293f3f66dd343630311e5f79fc79cdd53f9
langnerdavid <langner.david@outlook.com>
2023-12-10 09:48:30 +0100

    create article site but not styled

--- basic-frontend/src/app/app-routing.module.ts
+++ basic-frontend/src/app/app-routing.module.ts
@@ -4,22 +4,14 @@ import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.compo
 
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
-  },
-  {
-    path: '404',
-    component: PageNotFoundComponent
-  },
+  { path: '', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: '404', component: PageNotFoundComponent},
   { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
   { path: 'signin', loadChildren: () => import('./pages/signin/signin.module').then(m => m.SigninModule) },
   { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
   { path: 'basket', loadChildren: () => import('./pages/basket/basket.module').then(m => m.BasketModule) },
-  {
-    path: '**',
-    redirectTo: '404'
-  }
+  { path: 'article', loadChildren: () => import('./pages/article/article.module').then(m => m.ArticleModule) },
+  { path: '**', redirectTo: '404'}
 ];
 
 @NgModule({
