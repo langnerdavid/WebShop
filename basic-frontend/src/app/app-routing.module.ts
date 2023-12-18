1818baaddb3873e7dec59781d990eaa089b71e67
langnerdavid <langner.david@outlook.com>
2023-12-18 11:30:35 +0100

    article register

--- basic-frontend/src/app/app-routing.module.ts
+++ basic-frontend/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'signin', loadChildren: () => import('./pages/signin/signin.module').then(m => m.SigninModule) },
   { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
   { path: 'basket', loadChildren: () => import('./pages/basket/basket.module').then(m => m.BasketModule) },
+  { path: 'article', loadChildren: () => import('./pages/article/article.module').then(m => m.ArticleModule) },
   { path: '**', redirectTo: '404'}
 ];
 
